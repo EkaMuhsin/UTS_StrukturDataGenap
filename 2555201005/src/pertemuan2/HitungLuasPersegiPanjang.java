bdca3e4a28b966657cfcd4ff38274740a7094ef2
EkaMuhsin <ekadansania@gmail.com>
2026-03-31 20:49:15 +0700

    first commit

---
+++ 2555201005/src/pertemuan2/HitungLuasPersegiPanjang.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan2;
+
+/**
+ *
+ * @author eka
+ */
+public class HitungLuasPersegiPanjang {
+    public static void main(String[] args) {
+        int Panjang=20;
+        int Lebar=26;
+        int Luas=Panjang*Lebar;
+        System.out.println("Hitung Luas Persegi Panjang");
+        System.out.println("Panjang :"+Panjang);
+        System.out.println("Lebar :"+Lebar);
+        System.out.println("Luas :"+Luas);
+    }
+}
